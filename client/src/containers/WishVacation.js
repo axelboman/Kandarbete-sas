3a6461985e7e4f543e5b2e52d9815a28f631bc24
axelboman <axelbomans.gmail.com>
2018-04-17 11:28:55 +0200

    daypickerinput

--- client/src/containers/WishVacation.js
+++ client/src/containers/WishVacation.js
@@ -2,92 +2,157 @@ import React, { Component } from "react";
 
 import "./WishVacation.css";
 
-import Calendar from 'react-calendar';
-import DatePicker from 'react-date-picker'
 
+import moment from 'moment';
+import {RadioButton, RadioGroup} from 'react-radio-buttons'
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
 
-
+import { formatDate, parseDate } from 'react-day-picker/moment';
 
 export default class WishVacation extends Component {
-
-    //this is for the calendar section
-    state = {
-      date: new Date(),
-      }
-      onChange = date => this.setState({ date })
-    
-      //This is where the content is rendered.
-    render() {
-      return (
-        <div className="body">
-        <div className="container"> 
-          <div className="datePickerSection" >
-            <div className="handChoice">
-              <label>1 First hand choice <br/> </label>
-              <label>From: </label>
-                <DatePicker className="datePicker"
-                  onChange={this.onChange}
-                  value={this.state.date}
-                />
-              <label>To: </label>
-                <DatePicker className="datePicker"
-                  onChange={this.onChange}
-                  value={this.state.date}
-                />
-      
-              </div>
-      
-              <div className="handChoice">
-              <label>2 Second hand choice <br/> </label>
-              <label>From: </label>
-                <DatePicker className="datePicker"
-                  onChange={this.onChange}
-                  value={this.state.date}
-                />
-              <label>To: </label>
-                <DatePicker className="datePicker"
-                  onChange={this.onChange}
-                  value={this.state.date}
-                />
-      
-              </div>
-      
-              <div className="handChoice">
-              <label>3 third hand choice <br/> </label>
-              <label>From: </label>
-                <DatePicker className="datePicker"
-                  onChange={this.onChange}
-                  value={this.state.date}
-                />
-              <label>To: </label>
-                <DatePicker className="datePicker"
-                  onChange={this.onChange}
-                  value={this.state.date}
-                />
-      
-              </div>
-          </div>
-      
-          {/* this is where we create the calenders */}
-          <div className="calendarSection">
-            <div className="innerCalendar1">
-              <Calendar className="celenderStyle"
-                onChange={this.onChange}
-                value={this.state.date}
-              />
-            </div>
-            <div className="innerCalendar2">
-              <Calendar className="celenderStyle"
-                onChange={this.onChange}
-                value={this.state.date}
-              />
-            </div>
-          </div>
-      
-        </div>
-      </div>
-      );
+  constructor(props) {
+    super(props);
+    this.handleFromChange = this.handleFromChange.bind(this);
+    this.handleToChange = this.handleToChange.bind(this);
+    this.state = {
+      from: undefined,
+      to: undefined,
+    };
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+  focusTo() {
+    // Focus to `to` field. A timeout is required here because the overlays
+    // already set timeouts to work well with input fields
+    this.timeout = setTimeout(() => this.to.getInput().focus(), 0);
+  }
+  showFromMonth() {
+    const { from, to } = this.state;
+    if (!from) {
+      return;
+    }
+    if (moment(to).diff(moment(from), 'months') < 2) {
+      this.to.getDayPicker().showMonth(from);
     }
   }
+  handleFromChange(from) {
+    // Change the from date and focus the "to" input field
+    this.setState({ from }, () => {
+      if (!this.state.to) {
+        this.focusTo();
+      }
+    });
+  }
+  handleToChange(to) {
+    this.setState({ to }, this.showFromMonth);
+  }
 
+  render() {
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
+    return (
+    <div className="body">  
+
+      <div className="selection">
+            <RadioGroup onChange={ this.onChange } horizontal>
+              <RadioButton value="longtime">Long time</RadioButton>
+              <RadioButton value="shorttime">Short time</RadioButton>
+            </RadioGroup>
+      </div>
+      <div className="container">
+      
+        <div className="firstHandChoice">
+          <div className="InputFromTo">
+            <DayPickerInput
+              value={from}
+              placeholder="From"
+              format="LL"
+              formatDate={formatDate}
+              parseDate={parseDate}
+              dayPickerProps={{
+                selectedDays: [from, { from, to }],
+                disabledDays: { after: to },
+                toMonth: to,
+                modifiers,
+                numberOfMonths: 2,
+              }}
+              onDayChange={this.handleFromChange}
+            />{' '}
+            —{' '}
+            <span className="InputFromTo-to">
+              <DayPickerInput
+                ref={el => (this.to = el)}
+                value={to}
+                placeholder="To"
+                format="LL"
+                formatDate={formatDate}
+                parseDate={parseDate}
+                dayPickerProps={{
+                  selectedDays: [from, { from, to }],
+                  disabledDays: { before: from },
+                  modifiers,
+                  month: from,
+                  fromMonth: from,
+                  numberOfMonths: 2,
+                }}
+                onDayChange={this.handleToChange}
+              />
+            </span>
+          </div>
+        </div> 
+        <div className="secondHandChoice">      
+          <div className="InputFromTo">
+            <DayPickerInput
+              value={from}
+              placeholder="From"
+              format="LL"
+              formatDate={formatDate}
+              parseDate={parseDate}
+              dayPickerProps={{
+                selectedDays: [from, { from, to }],
+                disabledDays: { after: to },
+                toMonth: to,
+                modifiers,
+                numberOfMonths: 2,
+              }}
+              onDayChange={this.handleFromChange}
+            />{' '}
+            —{' '}
+            <span className="InputFromTo-to">
+              <DayPickerInput
+                ref={el => (this.to = el)}
+                value={to}
+                placeholder="To"
+                format="LL"
+                formatDate={formatDate}
+                parseDate={parseDate}
+                dayPickerProps={{
+                  selectedDays: [from, { from, to }],
+                  disabledDays: { before: from },
+                  modifiers,
+                  month: from,
+                  fromMonth: from,
+                  numberOfMonths: 2,
+                }}
+                onDayChange={this.handleToChange}
+              />
+            </span>
+          </div>
+        </div>    
+      </div>
+    </div>
+    );
+  }
+} 
+            
+          
+
+        
+          
+        
+        
+        
+    
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 ReactDOM.render(
     <Router>
     <App />
