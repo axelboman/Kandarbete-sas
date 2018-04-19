46b4b180a156f810c3de160e404ba57c53fdce56
womba-womba <armin.zakeri@gmail.com>
2018-04-19 16:54:11 +0200

    dded mysql

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+var mysql = require('mysql');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "staralliance",
+  database: "mydb"
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
