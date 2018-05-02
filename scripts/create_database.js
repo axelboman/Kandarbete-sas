9b44cf7201c2fef2537225f50b3d0bc54d1b0ae5
womba-womba <armin.zakeri@gmail.com>
2018-05-02 14:59:37 +0200

    changed script

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,13 +1,36 @@
 var mysql = require('mysql');
 var options = require("../server/database").database;
+var bcrypt = require('bcrypt');
 var con = mysql.createConnection(options);
+var myArray = ['Stockholm', 'Oslo', 'Copenhagen'];   
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password BINARY(60))";
-  con.query(sql, function (err, result) {
+bcrypt.hash('staralliance', 10, function (err, hash) {
+  con.connect(function (err) {
     if (err) throw err;
-    console.log("Table created");
+    console.log("Connected!");
+    var sql = "update users set password = ? where emp_no NOT LIKE '1002'";
+    con.query(sql, [hash], function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
   });
 });
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "delete from users where emp_no not like '1002'";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password BINARY(60))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
